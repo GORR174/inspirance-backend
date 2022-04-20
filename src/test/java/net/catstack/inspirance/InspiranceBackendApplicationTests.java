37e508988264750271d2292d340e1755bc95cf15
Gorr <gorrgorrob@gmail.com>
2022-04-20 15:22:16 +0500

    Hotfix

--- src/test/java/net/catstack/inspirance/InspiranceBackendApplicationTests.java
+++ src/test/java/net/catstack/inspirance/InspiranceBackendApplicationTests.java
@@ -3,11 +3,10 @@ package net.catstack.inspirance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InspiranceBackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test() {
     }
 
 }
